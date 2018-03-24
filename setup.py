52f8043e3e31df9cdf205c523e9abb5da43b288e
niharika <niharika.gauraha@farmbio.uu.se>
2018-03-24 10:25:54 +0100

    initial SVM+ implementation

---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+setup(
+    name='svmplus',
+    version='0.1',
+    packages=['svmplus'],
+    url='',
+    license='',
+    author='niharika',
+    author_email='niharika.gauraha@farmbio.uu.se',
+    description='Implementation of SVM+'
+)
---
+++ svmplus/__init__.py
@@ -0,0 +1,12 @@
+"""
+The :mod:`svmplus` module includes SVM plus algorithms.
+"""
+
+# See http://scikit-learn.sourceforge.net/modules/svm.html for complete
+# documentation.
+
+# Authors: Niharika Gauraha<niharika.gauraha@farmbio.uu.se>
+#         Ola Spjuth<ola.spjuth@farmbio.uu.se>
+# License:
+
+__all__ = ['SVMPlus']
